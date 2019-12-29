663aacabeab03962750c1168a237f70f37403552
MvexD <muwiewow@gmail.com>
2019-12-29 23:25:31 +0100

    29.12.2019    23:24 || fixed dbhelper issues

--- app/src/main/java/com/example/quizapp/Model/Question.kt
+++ app/src/main/java/com/example/quizapp/Model/Question.kt
@@ -3,10 +3,9 @@ package com.example.quizapp.Model
 class Question (
     var id:Int,
     var questionText:String?,
-    var questionImage:String?,
     var answerA:String?,
     var answerB:String?,
     var answerC:String?,
     var answerD:String?,
-    var correctAnswer:String?,
+    var correctAnswer:String?
 )
